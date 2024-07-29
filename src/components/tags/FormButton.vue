b6722c96e60cbcea3ed426beed05a8b206ef386c
dilshod olimjonov <dilshodlv@icloud.com>
2024-07-29 21:42:51 +0500

    create Form & Login components

---
+++ src/components/tags/FormButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+defineProps({
+    name: {
+        type: String,
+        required: true
+    },
+    loading:{
+        type:Boolean,
+        default: false
+    },
+})
+</script>
+
+<template>
+    <button type="button" class="btn" style="min-width: 35%">
+        <span>{{name}}</span>
+        <span v-if="loading" class="spinner-border spinner-border-sm text-light"></span>
+    </button>
+</template>
+
+<style scoped>
+
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,22 @@
 <script setup>
 
+import FormInput from "@/components/tags/FormInput.vue";
+import FormButton from "@/components/tags/FormButton.vue";
 </script>
 
 <template>
-    <div class="bg-warning">Login Sahifasi</div>
+    <div class="row justify-content-center align-items-center vh-100">
+        <div class="col-12 col-sm-8 col-md-6 col-xl-4">
+            <h1 class="text-center">Kirish</h1>
+            <form>
+                <FormInput props-id="email" input-type="email" label-name="Email" />
+                <FormInput props-id="password" input-type="password" label-name="Parol" />
+                <div class="text-end">
+                    <FormButton name="Kirish" class="btn-primary" />
+                </div>
+            </form>
+        </div>
+    </div>
 </template>
 
 <style scoped>
